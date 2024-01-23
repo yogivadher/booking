48bccd932291405a29c578fcfce3e637275cdbf3
vadher_yogesh vadher_yogesh <you@example.com>
2024-01-23 11:55:25 +0000

    commited modification in controller

--- src/main/java/com/cinema/booking/controller/BookingController.java
+++ src/main/java/com/cinema/booking/controller/BookingController.java
@@ -13,6 +13,7 @@ public class BookingController {
    // @RequestMapping(path="/tickets", method= RequestMethod.POST)
     @PostMapping(path="/tickets")
     public Ticket addTicket(@RequestBody Ticket ticket){
+        System.out.println("controller called");
         ticketList.add(ticket);
         return ticket;
     }
